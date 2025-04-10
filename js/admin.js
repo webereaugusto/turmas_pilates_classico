652047e47c4838c23fee513f9bb1c6ca44e3cd00
Seu Nome <seu.email@exemplo.com>
2025-04-10 15:16:16 -0300

    Adicionado arquivo admin.js faltante

---
+++ js/admin.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function($) {
+    // Funções administrativas aqui
+    // Por enquanto este arquivo pode ficar vazio pois as funcionalidades
+    // específicas do admin estão sendo tratadas inline nos callbacks dos meta boxes
+}); 
